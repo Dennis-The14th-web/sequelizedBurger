30d6c7fb22faba927e5be7662b8b2aae84776aa4
the 14th web <dennis.itua1@gmail.com>
2019-12-02 12:21:30 -0500

    updating details

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quater Pounder Cheeseburger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac Cheeseburger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Junior Cheeseburger', FALSE);
